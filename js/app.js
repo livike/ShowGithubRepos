9f3a12cddd8847057dccc26f03506b3c35b84177
Livia Dobai <livia.dobai@gmail.com>
2017-09-09 22:57:26 +0200

    Upgraded the whole structure. Added styles.

--- js/app.js
+++ js/app.js
@@ -6,79 +6,100 @@ const user = "addyosmani";
 let number_of_pages1;
 let nextrepo;
 
-function createNode (element) {
-   return document.createElement(element); // Create the type of element you pass in the parameters
- }
 
- function append (parent, el) {
-   return parent.appendChild(el); // Append the second parameter(element) to the first one
- }
-
- function parseResults (response){
-   const myNodeUl = document.getElementById("reposit");// Select the element by its ID
-   //First remove all the li child elements if any
-   while (myNodeUl.firstChild) {
-    myNodeUl.removeChild(myNodeUl.firstChild);
-}
-//   debugger;
-    let repos = response; // Get the results
-    return repos.map(function(repo) { // Map through the results and for each run the code below
-      let li = createNode('li'), //  Create the elements we need
-          span = createNode('span');
-      span.innerHTML = repo.name; // Make the HTML of our span to be the first and last name of our author
-      // Append all our elements
-      append(li, span);
-      append(myNodeUl, li);
- })
+function createRepoList(response){
+  //Prepare the data for the Mustache template
+  let i=0;
+  response.map(function(item){
+    item.index = i;
+    i+=1;
+    const e = function(){
+      if(item.language){
+        return true;
+      } else {
+        return false;
+      }
+    };
+    item.langexist = e;
+  });
+  let repos = $.extend([], response);
+  repos = {"repos": repos};
+  const template = $("#ghRepos").html().trim();
+  //Render the Mustache template
+  let html = Mustache.render(template,repos);
+  $("#reposit").html(html);
 }
 
 function numberOfPages(link){
   const links = {};
-
   if (link) {
     link.replace(/<([^>]*)>;\s*rel="([\w]*)\"/g, function(m, uri, type) {
       links[type] = uri;
     });
   }
-
   const repolinks = links; //Set a links attribute on this collection, may look different in your framework
-  // nextrepo = repolinks.next;
-  // debugger;
   const a = repolinks.last.lastIndexOf('=');
   const b = repolinks.last.length;
   navigationHtml(repolinks.last.slice(a+1,b));
   return repolinks.last.slice(a+1,b);
 }
-function githubUser(){
-  const urlallrepos = baseurl+user+"/repos"; // Get 10 random users
- // const urlrepo = 'https://api.github.com/repos/'+user;
-  githubRepos(urlallrepos);
-  }
 
 function githubRepos (url){
 
   fetch(url)
   .then(function(response) {
-//debugger;
   if(!number_of_pages1){
       number_of_pages1 = numberOfPages(response.headers.get('link'));// getting the number of pages of all repos
   }
-
     return response.json();
-    //debugger;
-
   })
-  .then(parseResults)
+  .then(createRepoList)
   .catch(function(error) {
-      console.log("Hiba: "+error);
+      console.log("gitubRepos - "+error);
     });
   }
 
+  function githubUser(){
+    const urlrepo = 'https://api.github.com/users/'+user;
+
+    fetch(urlrepo)
+    .then(resp => resp.json())
+    .then(function(resp){
+      const template = $("#ghUser").html().trim();
+      //Prepare the data for the Mustache template
+      let data = {
+        name: resp.name,
+        avatar_url: resp.avatar_url,
+        login: resp.login,
+        bio: resp.bio,
+        company: resp.company,
+        location: resp.location,
+        blog: resp.blog,
+        followers: resp.followers,
+        following: resp.following,
+        public_repos: resp.public_repos
+      }
+      //Render the Mustache template
+      let html = Mustache.render(template,data);
+      $("#userArea").html(html);
+    })
+    .catch(function(error){
+      console.log("githubUser - "+error)
+    })
+    //Fetch and show the repos fot the user
+    const urlallrepos = baseurl+user+"/repos";
+    githubRepos(urlallrepos);
+    }
+
+//////////////
+//NAVIGATION//
+/////////////
 function navigationHtml(number_of_pages){
-  //debugger;
+  //set the hidden field "current_page to 1"
   let current_page = document.getElementById("current_page");
   current_page.value = 1;
 
+  //create the navigation node
   let navigation_html = '<a class="previous_link" href="javascript:previous();"> Prev </a>';
   let current_link = 1;
   while(number_of_pages >= current_link){
@@ -91,25 +112,24 @@ function navigationHtml(number_of_pages){
 
   //add active_page class to the first page link
   $('#page_navigation .page_link:first').addClass('active_page');
-  }
+}
 
-  function go_to_page(page_num){
-    //debugger;
-    githubRepos(baseurl+user+"/repos?page="+page_num);
+function go_to_page(page_num){
+  //debugger;
+  githubRepos(baseurl+user+"/repos?page="+page_num);
+  //set the actual active page class
+  $('.page_link[longdesc=' + page_num +']').addClass('active_page').siblings('.active_page').removeClass('active_page');
+  $('#current_page').val(page_num);//update the current page input field
 
-  	//update the current page input field
-    let current_page = document.getElementById("current_page");
-    current_page.value = page_num;
-  }
+}
 
-  function previous(){
+function previous(){
 
 	let new_page = parseInt($('#current_page').val()) - 1;
 	//if there is an item before the current active link run the function
 	if($('.active_page').prev('.page_link').length==true){
 		go_to_page(new_page);
 	}
-
 }
 
 function next(){
@@ -118,7 +138,9 @@ function next(){
 	if($('.active_page').next('.page_link').length==true){
 		go_to_page(new_page);
 	}
-
 }
+//////////////////
+//END NAVIGATION//
+/////////////////
 
   githubUser();
